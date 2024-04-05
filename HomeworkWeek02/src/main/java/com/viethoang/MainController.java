891f33b299ae33f6f85088d923974ac8b51a175d
nvhoang295 <hoangnv.swe@gmail.com>
2024-04-05 21:54:48 +0700

    submit hw week3

--- HomeworkWeek02/src/main/java/com/viethoang/MainController.java
+++ HomeworkWeek02/src/main/java/com/viethoang/MainController.java
@@ -1,5 +1,6 @@
 package com.viethoang;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Controller;
@@ -7,9 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+	List<User> users = new ArrayList<>(List.of(
+				User.builder().name("Hoang").gender("male").build(), 
+				User.builder().name("Duong").gender("male").build(), 
+				User.builder().name("Kien").gender("male").build()));
 	
 	@GetMapping("/login")
 	public String login(Model model) {
@@ -23,19 +29,23 @@ public class MainController {
 			@RequestParam("password") String password,
 			Model model) {
 		if (username.equals("viethoang") && password.equals("1234")) {
+			// nó return về thằng page chứ không return về get mapping method viết ở dưới nên phải add attribute vào
+			model.addAttribute("users", users);
 			return "home";
 		}
 		model.addAttribute("message", "Đăng nhập thất bại!");
 		return "redirect:login";
-		
 	}
 	
 	@GetMapping("/home")
 	public String listUsers(Model model) {
-		model.addAttribute("users", List.of(
-				User.builder().name("Hoang").gender("male").build(), 
-				User.builder().name("Duong").gender("male").build(), 
-				User.builder().name("Kien").gender("male").build()));
+		model.addAttribute("users", users);
 		return "home";
+	}
+	
+	@GetMapping("/json")
+	@ResponseBody
+	public List<User> getUsersJsonFormat() {
+		return users;
 	}
 }
